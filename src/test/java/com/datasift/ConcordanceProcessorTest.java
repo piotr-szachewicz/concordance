f1b0c9800cf8443a5b5037b6195a7cf5d942cff3
Piotr Szachewicz <piotr.szachewicz@gmail.com>
2014-05-15 18:18:54 +0200

    Refactored the method for getting parameters in JUnit tests.

--- src/test/java/com/datasift/ConcordanceProcessorTest.java
+++ src/test/java/com/datasift/ConcordanceProcessorTest.java
@@ -3,12 +3,15 @@ package com.datasift;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.datasift.utils.ResourceUtils;
 
@@ -18,6 +21,10 @@ public class ConcordanceProcessorTest {
 	protected String input;
 	protected String expectedOutput;
 	private ConcordanceProcessor concordanceProcessor;
+
+	protected static String[] TEST_FILE_BEGINNINGS = new String[] {
+			"one_sentence", "two_sentences", "abbreviation", "quotation_marks",
+			"word_with_a_hyphen", "long_text", "appostrophes" };
 
 	public ConcordanceProcessorTest(String fileNameBeginning)
 			throws IOException {
@@ -30,6 +37,10 @@ public class ConcordanceProcessorTest {
 	@Test
 	public void testProcess() {
 		String actualOutput = processInput();
+
+		// System.out.print("EXPECTED=" + expectedOutput + "#");
+		// System.out.print("ACTUAL=" + actualOutput + "#");
+
 		assertEquals(expectedOutput, actualOutput);
 	}
 
@@ -37,13 +48,13 @@ public class ConcordanceProcessorTest {
 		return concordanceProcessor.process(input);
 	}
 
-	@Parameterized.Parameters
-	public static Collection getFileNameBeginnings() {
-		return Arrays
-				.asList(new Object[][] { { "one_sentence" },
-						{ "two_sentences" }, { "abbreviation" },
-						{ "quotation_marks" }, { "word_with_a_hyphen" },
-						{ "long_text" }, { "appostrophes" } });
+	@Parameters(name = "{index}: {0})")
+	public static Collection<String[]> getFileNameBeginnings() {
+		List<String[]> parameters = new ArrayList<String[]>();
+		for (String fileBeginning : TEST_FILE_BEGINNINGS) {
+			parameters.add(new String[] { fileBeginning });
+		}
+		return parameters;
 	}
 
 }
